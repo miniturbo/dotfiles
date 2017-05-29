1c4cd8929e98029f8bfc57e2329bef5d417dc15b
Kazuya Shono <kazuya.shono@gmail.com>
2017-05-29 10:08:41 +0900

    add homebrew cookbook to darwin role

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -3,6 +3,7 @@ include_recipe '../../helpers/recipe_helper'
 node.reverse_merge!(user: ENV['SUDO_USER'] || ENV['USER'])
 
 include_cookbook 'functions'
+include_cookbook 'homebrew'
 include_cookbook 'zsh'
 include_cookbook 'tmux'
 include_cookbook 'vim'
