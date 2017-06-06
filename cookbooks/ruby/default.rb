3458bd3fc4d741210b33345e1e1907dd7fdfc6da
Kazuya Shono <kazuya.shono@gmail.com>
2017-06-06 13:43:06 +0900

    add ruby cookbook to linux role

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,3 +1,24 @@
-%w(libffi libyaml openssl).each { |name| package name }
+
+if node[:platform] == 'darwin'
+  %w(libffi libyaml openssl).each { |name| package name }
+else
+  %w(
+    autoconf
+    bison
+    build-essential
+    libffi-dev
+    libgdbm-dev
+    libgdbm3
+    libncurses5-dev
+    libreadline6-dev
+    libssl-dev
+    libyaml-dev
+    zlib1g-dev
+  ).each do |name|
+    package name do
+      user 'root'
+    end
+  end
+end
 
 dotfile '.gemrc'
