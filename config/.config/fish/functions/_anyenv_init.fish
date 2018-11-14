9055254d34f38977a1e199348114d54adfe75d33
Kazuya Shono <kazuya.shono@gmail.com>
2018-11-14 13:57:08 +0900

    set **ENV_ROOT as global variables

--- config/.config/fish/functions/_anyenv_init.fish
+++ config/.config/fish/functions/_anyenv_init.fish
@@ -1,6 +1,6 @@
 function _anyenv_init
   _set_path $HOME/.anyenv/bin
-  builtin source (anyenv init - | psub)
+  builtin source (anyenv init - | string replace -r 'set -x ([A-Z]+ENV_ROOT)' 'set -gx $1' | psub)
 
   # fix **env/bin path position and deduplicate path
   set -l new_path
