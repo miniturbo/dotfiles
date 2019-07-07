481cd7567499d5be51422fbdb3adf5852887cc02
Kazuya Shono <kazuya.shono@gmail.com>
2019-07-08 00:03:43 +0900

    switch hyper to iterm2

--- cookbooks/iterm2/default.rb
+++ cookbooks/iterm2/default.rb
@@ -1,6 +1,25 @@
-cask 'iterm2'
+execute 'add homebrew/cask-versions repository' do
+  command 'brew tap homebrew/cask-versions'
+  not_if 'env HOMEBREW_NO_AUTO_UPDATE=1 brew tap | grep -q homebrew/cask-versions'
+end
 
-file = "#{ENV['HOME']}/Library/Preferences/com.googlecode.iterm2.plist"
-remote_file file do
-  not_if "test -f #{file}"
+cask 'iterm2-beta'
+
+dotfile '.config/iterm2'
+
+execute 'set PrefsCustomFolder' do
+  dir = '~/.config/iterm2'
+  command <<-EOF
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string '~/.config/iterm2'
+    killall cfprefsd
+  EOF
+  not_if 'defaults read com.googlecode.iterm2.plist PrefsCustomFolder'
+end
+
+execute 'set LoadPrefsFromCustomFolder' do
+  command <<-EOF
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+    killall cfprefsd
+  EOF
+  not_if 'defaults read com.googlecode.iterm2.plist LoadPrefsFromCustomFolder'
 end
