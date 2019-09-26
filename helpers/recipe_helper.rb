88f7d800fb3a275a241865ed926dd21fbb5396c3
Kazuya Shono <kazuya.shono@gmail.com>
2019-09-26 16:48:17 +0900

    refactor

--- helpers/recipe_helper.rb
+++ helpers/recipe_helper.rb
@@ -3,9 +3,4 @@ MItamae::RecipeContext.class_eval do
     root_dir = File.expand_path('../..', __FILE__)
     include_recipe File.join(root_dir, 'cookbooks', name, 'default')
   end
-
-  def include_role(name)
-    root_dir = File.expand_path('../..', __FILE__)
-    include_recipe File.join(root_dir, 'roles', name, 'default')
-  end
 end
--- roles/base/default.rb
+++
@@ -1,3 +0,0 @@
-node.reverse_merge!(user: ENV['SUDO_USER'] || ENV['USER'])
-
-include_cookbook 'functions'
