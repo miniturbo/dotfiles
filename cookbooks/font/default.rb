26a16c4f144bff8af716ca652430878e3c5838e4
Kazuya Shono <kazuya.shono@gmail.com>
2020-07-19 22:40:57 +0900

    feat: fontクックブックを追加した

---
+++ cookbooks/font/default.rb
@@ -0,0 +1,12 @@
+execute 'install HackGen' do
+  url = 'https://github.com/yuru7/HackGen/releases/download/v2.0.0/HackGen_v2.0.0.zip'
+  command <<-EOS
+    curl -sL -o HackGen.zip #{url}
+    unzip -j -d HackGen HackGen.zip
+    cp HackGen/*.ttf $HOME/Library/Fonts/
+    rm -rf HackGen*
+  EOS
+  cwd '/tmp'
+  user node[:user]
+  not_if 'test -f $HOME/Library/Fonts/HackGen-Regular.ttf'
+end
