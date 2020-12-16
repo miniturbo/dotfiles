88776eb94359490ef8e2cff177d920a3e914f55d
miniturbo <kazuya.shono@gmail.com>
2020-12-17 01:42:04 +0900

    feat: fisher 4.x系に合わせて構成を変更した

--- cookbooks/fish/default.rb
+++ cookbooks/fish/default.rb
@@ -2,9 +2,15 @@ fish_path = '/usr/local/bin/fish'
 
 package 'fish'
 
-dotfile '.config/fish/conf.d'
-dotfile '.config/fish/fishfile'
-dotfile '.config/fish/functions'
+dotfile '.config/fish/conf.d/00_env.fish'
+dotfile '.config/fish/conf.d/10_color.fish'
+dotfile '.config/fish/conf.d/20_alias.fish'
+dotfile '.config/fish/conf.d/30_tool.fish'
+dotfile '.config/fish/conf.d/90_fisher.fish'
+dotfile '.config/fish/fish_plugins'
+dotfile '.config/fish/functions/_set_path.fish'
+dotfile '.config/fish/functions/_tmux_auto_rename_session.fish'
+dotfile '.config/fish/functions/_tmux_generate_session_name.fish'
 
 execute 'append fish path to /etc/shells' do
   command "echo #{fish_path} | sudo tee -a /etc/shells"
@@ -14,4 +20,9 @@ end
 execute 'change shell to fish' do
   command "sudo chsh -s #{fish_path} #{node[:user]}"
   not_if %Q|[ "$SHELL" = "#{fish_path}" ]|
+end
+
+execute 'install fisher' do
+  command %q[fish -c 'curl -sL https://git.io/fisher | source && fisher update']
+  not_if "test -f #{ENV['HOME']}/.config/fish/functions/fisher.fish"
 end
