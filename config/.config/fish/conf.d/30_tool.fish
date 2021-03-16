cd363c91abfcb1c4969ceb102b224cc4231698a8
Kazuya Shono <kazuya.shono@gmail.com>
2021-03-16 10:18:20 +0900

    fix: google-cloud-sdk向けの設定を更新した

--- config/.config/fish/conf.d/30_tool.fish
+++ config/.config/fish/conf.d/30_tool.fish
@@ -23,6 +23,6 @@ if status is-interactive
   # google-cloud-sdk
   if test -d /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
     _set_path /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
-    set -x CLOUDSDK_PYTHON /usr/local/opt/python@3.8/libexec/bin/python
+    set -x CLOUDSDK_PYTHON /usr/local/opt/python@3.9/libexec/bin/python
   end
 end
