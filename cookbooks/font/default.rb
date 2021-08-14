dd643872c5f9765afab1508448754de97e5dc0d4
miniturbo <kazuya.shono@gmail.com>
2021-08-14 18:37:49 +0900

    feat: HackGenNerdをインストールするようにした

--- cookbooks/font/default.rb
+++ cookbooks/font/default.rb
@@ -10,3 +10,16 @@ execute 'install HackGen' do
   user node[:user]
   not_if 'test -f $HOME/Library/Fonts/HackGen-Regular.ttf'
 end
+
+execute 'install HackGenNerd' do
+  url = 'https://github.com/yuru7/HackGen/releases/download/v2.4.1/HackGenNerd_v2.4.1.zip'
+  command <<-EOS
+    curl -sL -o HackGenNerd.zip #{url}
+    unzip -j -d HackGenNerd HackGenNerd.zip
+    cp HackGenNerd/*.ttf $HOME/Library/Fonts/
+    rm -rf HackGenNerd*
+  EOS
+  cwd '/tmp'
+  user node[:user]
+  not_if 'test -f $HOME/Library/Fonts/HackGenNerd-Regular.ttf'
+end
