117dd0e33e0b3d6736552c54140d23f01969c5da
miniturbo <kazuya.shono@gmail.com>
2022-10-30 23:45:28 +0900

    feat: 白源をアップグレードした

--- cookbooks/font/default.rb
+++ cookbooks/font/default.rb
@@ -1,25 +1,13 @@
-execute 'install HackGen' do
-  url = 'https://github.com/yuru7/HackGen/releases/download/v2.4.1/HackGen_v2.4.1.zip'
-  command <<-EOS
-    curl -sL -o HackGen.zip #{url}
-    unzip -j -d HackGen HackGen.zip
-    cp HackGen/*.ttf $HOME/Library/Fonts/
-    rm -rf HackGen*
-  EOS
-  cwd '/tmp'
-  user node[:user]
-  not_if 'test -f $HOME/Library/Fonts/HackGen-Regular.ttf'
+execute 'tap homebrew/cask-fonts' do
+  command 'brew tap homebrew/cask-fonts'
+  not_if 'brew tap | grep homebrew/cask-fonts'
 end
 
-execute 'install HackGenNerd' do
-  url = 'https://github.com/yuru7/HackGen/releases/download/v2.4.1/HackGenNerd_v2.4.1.zip'
-  command <<-EOS
-    curl -sL -o HackGenNerd.zip #{url}
-    unzip -j -d HackGenNerd HackGenNerd.zip
-    cp HackGenNerd/*.ttf $HOME/Library/Fonts/
-    rm -rf HackGenNerd*
-  EOS
-  cwd '/tmp'
-  user node[:user]
-  not_if 'test -f $HOME/Library/Fonts/HackGenNerd-Regular.ttf'
+%w[
+  font-hackgen
+  font-hackgen-nerd
+].each do |name|
+  package name do
+    not_if "brew list | grep #{name}"
+  end
 end
